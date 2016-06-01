d1912c57d36c8173fb400b2d117bdd5dce178e27
Piotr Węglewski <190160@edu.p.lodz.pl>
2016-06-01 05:49:31 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name januszeInterfejsowApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the januszeInterfejsowApp
+ */
+angular.module('januszeInterfejsowApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
